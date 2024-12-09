32d9bd9b3fac431b508654b0fd0298d5b114bc31
mdgolamfardin <69181182+mdgolamfardin@users.noreply.github.com>
2024-12-09 06:17:54 -0400

    Major changed: moved flora fauna to ecosystem, more changes to be made.

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -5,29 +5,29 @@ import Images from "./subcomponents/Images";
 
 // Define the Gallery component as a functional component
 const Gallery = () => {
-    return (
-        // Wrapper div with responsive classes
-        <div className="flex flex-col items-center text-center px-4 sm:px-8 md:px-12 lg:px-28 2xl:px-40">
-            {/* Heading */}
-            <h1 className="break-normal text-4xl lg:text-5xl font-semibold text-headingClr mt-8 mb-4">
-                Gallery
-            </h1>
-            
-            {/* Images, this is the component you, chatgpt, write for me */}
-            <div className="w-full">
-                <Images /> 
-            </div>
+  return (
+    // Wrapper div with responsive classes
+    <div className="flex flex-col items-center text-center px-4 sm:px-8 md:px-12 lg:px-28 2xl:px-40">
+      {/* Heading */}
+      <h1 className="break-normal text-4xl lg:text-5xl font-semibold text-headingClr mt-8 mb-4">
+        Gallery
+      </h1>
+      <h2
+        className="text-xl lg:text-2xl py-5  font-bold  no-shadow"
+      >
+        Take a look at the stunning photos taken right here.
+      </h2>
 
-            {/* Subcomponents */}
-            <div className="w-full">
-                <FloraFaunaFungi />
-            </div>
+      {/* Images, this is the component you, chatgpt, write for me */}
+      <div className="w-full">
+        <Images />
+      </div>
 
-            <div className="w-full mt-8">
-                <ImgUpload />
-            </div>
-        </div>
-    );
+      <div className="w-full mt-8">
+        <ImgUpload />
+      </div>
+    </div>
+  );
 };
 
 // Export the Gallery component so it can be used in other parts of the app
