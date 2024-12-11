8cbfd6c3353f6d4d62c4ee3199074afe1334f8a6
mdgolamfardin <69181182+mdgolamfardin@users.noreply.github.com>
2024-12-10 21:28:14 -0400

    adj

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-// Import the FloraFaunaFungi and ImgUpload subcomponents from the subcomponents folder
-import FloraFaunaFungi from "./subcomponents/FloraFaunaFungi";
-import ImgUpload from "./subcomponents/ImgUpload";
-import React, { useEffect, useState } from 'react';
-import 'tailwindcss/tailwind.css';
-=======
 /**
  * Gallery Component
  * 
@@ -17,7 +10,6 @@ import 'tailwindcss/tailwind.css';
  * with styling adjustments for dark mode.
  * @author Md Golam Fardin
  */
->>>>>>> a084acec5f86c0a9a0fa130591b56033b1292a82
 
 // Import the ImgUpload and Images subcomponents for displaying and uploading images
 import ImgUpload from "./subcomponents/ImgUpload";
@@ -29,55 +21,6 @@ import { DarkModeContext } from "../DarkModeContext";
 
 // Define the Gallery component as a functional component
 const Gallery = () => {
-<<<<<<< HEAD
-     // Array of background image URLs
-  const images = [
-    'https://ugdev.cs.smu.ca/~group24H/images/flora-fauna/American%20Black%20Bear.jpg',
-    'https://ugdev.cs.smu.ca/~group24H/images/flora-fauna/White-tailed%20Deer.jpg',
-    'https://ugdev.cs.smu.ca/~group24H/images/flora-fauna/American%20Red%20Squirrel.jpeg',
-  ];
-
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    // Function to update the background image index
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // Change image every 5 seconds
-
-    // Clean up the interval on component unmount
-    return () => clearInterval(interval);
-  }, [images.length]);
-
-  return (
-    <div className="gallery">
-      <header
-        className="gallery-header h-64 flex-row items-center text-center lg:px-28 2xl:px-52"
-        style={{
-          backgroundImage: `url(${images[currentImageIndex]})`,
-          width: '100%',
-          height: '30vh',
-          backgroundSize: 'cover', 
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          transition: 'background-image 0.5s ease-in-out',
-          backgroundColor: 'beige',
-        }}
-      >
-        <h1 className="text-8xl font-bold">Woodland Gallery</h1>
-        <p className="text-lg mt-2">Explore the beauty of nature.</p>
-      </header>
-          
-        
-          {/* Render the FloraFaunaFungi subcomponent */}
-          <FloraFaunaFungi />
-        
-          {/* Render the ImgUpload subcomponent */}
-          <ImgUpload />
-        </div>
-    );
-}
-=======
   // Access the darkMode state from the DarkModeContext
   const { darkMode } = useContext(DarkModeContext);
 
@@ -115,7 +58,6 @@ const Gallery = () => {
     </div>
   );
 };
->>>>>>> a084acec5f86c0a9a0fa130591b56033b1292a82
 
 // Export the Gallery component so it can be used in other parts of the app
 export default Gallery;
